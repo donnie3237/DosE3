5ccbc97294248e6e14eb2016ae721b2f2036562c
SoRa <paradorn3237@gmail.com>
2023-07-06 19:43:37 +0700

    .

--- load/ActixRS.js
+++ load/ActixRS.js
@@ -3,7 +3,7 @@ import { runCommand } from "./process/runCommand.js";
 import { end } from "./process/end.js";
 
 function Actix_RS(name){
-    const gitActix_Mongodb = `git clone --depth 1 https://github.com/donnie3237/ExpressJS-Template.git ${name}`
+    const gitActix_Mongodb = `git clone --depth 1 https://github.com/donnie3237/Actix-web-template.git ${name}`
     runCommand(gitActix_Mongodb)
     Logo()
     end(name)
