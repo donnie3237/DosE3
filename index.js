a3ebeb0de6d0fa1277fd60f00326e8461efdb7e4
SoRa <paradorn3237@gmail.com>
2023-08-14 08:52:22 +0700

    .

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 #!/usr/bin/env node
-import inquirer from 'inquirer'
+import inquirer from 'inquirer';
 import chalk from 'chalk';
 import install from './install.js';
+import { runCommand } from './load/process/runCommand.js';
 
 const choices = [
-  chalk.rgb(0,0,255)('ReactTS'),
-  chalk.rgb(0,255,255)('SolidTS'),
+  chalk.rgb(0, 0, 255)('ReactTS'),
+  chalk.rgb(0, 255, 255)('SolidTS'),
   chalk.rgb(235, 1, 40)('AstroJS'),
   chalk.yellow('TauriJS'),
-  chalk.rgb(0,255,0)('ExpressTS'),
+  chalk.rgb(0, 255, 0)('ExpressTS'),
   chalk.rgb(84, 84, 84)('Actix-web'),
 ];
-inquirer.prompt(
-  [
-    {
-      type:"input",
-      name:"name",
-      message: "project name :",
-      default:"dose"
+
+const args = process.argv.slice(2);
+
+if (args.includes('update')) {
+  // Run npm install -g dose3@latest
+  runCommand('npm install -g dose3@latest', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
     }
-    ,{
-      type: "list",
-      name: "framework",
-      message: "Select framework of this project :",
-      choices
-    }
-  ]
-).then ( answers => {install(answers.framework , answers.name)});
+    console.log(stdout);
+  });
+} else if (args.includes('-v') || args.includes('--version')) {
+  console.log('Dose3 Version: 1.1.9-beta'); // Replace with your tool's name and version
+} else {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'project name :',
+        default: 'dose',
+      },
+      {
+        type: 'list',
+        name: 'framework',
+        message: 'Select framework of this project :',
+        choices,
+      },
+    ])
+    .then((answers) => {
+      install(answers.framework, answers.name);
+    });
+}
--- sora.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-console.log(('>>>>>>>>>>>>>>> Dose3 : version  1.1.8-beta            '))
