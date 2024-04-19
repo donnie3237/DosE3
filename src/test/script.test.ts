b14c2320f5090603a5dcc5d2f910e7b5438fb1a7
D0NN1E <paradorn3237@gmail.com>
2024-04-19 13:32:10 +0700

    .

--- src/test/script.test.ts
+++ src/test/script.test.ts
@@ -1,5 +1,11 @@
-import test from "node:test";
-
+import { runCommand } from "../load/process/runCommand";
+import {expec} from 'vitest'
 test("it is work fine", ()=>{
+    describe('')
+})
+describe('it should work with ',{ ()=>
+    it('should', ()=>{
+        runCommand( 'node -v')
+    })
     
 })
