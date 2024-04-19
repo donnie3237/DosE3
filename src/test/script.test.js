874eb15643dc320729810f48c12aae14c3ed6553
D0NN1E <paradorn3237@gmail.com>
2024-04-19 14:00:57 +0700

    .

--- src/test/script.test.js
+++ src/test/script.test.js
@@ -1,11 +1,6 @@
 import { runCommand } from "../load/process/runCommand";
-import {expect , test} from 'vitest'
+import {expect , test , vi} from 'vitest'
 
-function sum(num1 , num2){
-     const result = num1 + num2
-     return result
-}
 test('should not run WTF command', () => {
     expect(runCommand('cekodjgobivfdb')).toBe(false)
-    expect(runCommand('ls')).toBe(true)
 })
