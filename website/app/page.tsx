b82d5d6dde6b4a88f8e541988cec4a5bd8c98a2f
D0NN1E <paradorn3237@gmail.com>
2025-06-12 23:28:17 +0700

    .

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -8,7 +8,6 @@ import {
   Code2,
   TerminalSquare,
 } from 'lucide-react';
-import { CanvasRevealEffect } from "@/components/ui/canvas-reveal-effect";
 
 export default function Home() {
   return (
@@ -117,9 +116,6 @@ export default function Home() {
         <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
         Build amazing project <br /> at <Cover>speed of light</Cover>
       </h1>
-      </div>
-      <div className="w-[100%] h-[700px]">
-              <CanvasRevealEffect/>
       </div>
     </div>
   );
